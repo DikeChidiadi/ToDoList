ff1203eaebc0bf7f1be547817b95a949ef7eb7e2
Chidi <chidi.dike93@gmail.com>
2025-03-23 19:43:28 +0100

    chose something else

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,87 +1,61 @@
 <x-layout>
-    <x-slot:header>
-        <div class="flex gap-2 place-items-right">
-            <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
-            <div class="dropdown dropdown-end">
-                <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full">
-                    <img
-                        alt="Tailwind CSS Navbar component"
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                    </div>
-                </div>
-                <ul
-                    tabindex="0"
-                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li>
-                    <a class="justify-between">
-                        Profile
-                        <span class="badge">New</span>
-                    </a>
-                    </li>
-                    <li><a>Settings</a></li>
+    <form action="home" methode="post" class="flex flex-col space-y-12 justify-between">
+        @csrf
+        <div class="absolute top-0 right-0 p-2 mr-8">
+            <ul class="menu menu-horizontal px-1">
+            <li>
+                <details>
+                @auth
+                <summary>Hi, {{ Auth::user()->name }}</summary>
+                @endauth
+                <ul class="bg-base-100 rounded-t-none p-2">
                     <li><a>Logout</a></li>
                 </ul>
-            </div>
-        </div>
-    </x-slot:header>
-
-    <div>
-        <div>
-            <ul class="list bg-base-100 rounded-box shadow-md">
-                <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Most played songs this week</li>
-                
-                <li class="list-row">
-                    <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/1@94.webp"/></div>
-                        <div>
-                            <div>Dio Lupa</div>
-                            <div class="text-xs uppercase font-semibold opacity-60">Remaining Reason</div>
-                        </div>
-                        <p class="list-col-wrap text-xs">
-                        "Remaining Reason" became an instant hit, praised for its haunting sound and emotional depth. A viral performance brought it widespread recognition, making it one of Dio Lupa’s most iconic tracks.
-                        </p>
-                        <button class="btn btn-square btn-ghost">
-                            <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
-                        </button>
-                        <button class="btn btn-square btn-ghost">
-                            <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                        </button>
-                </li>
-                
-                <li class="list-row">
-                    <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/4@94.webp"/></div>
-                        <div>
-                            <div>Ellie Beilish</div>
-                            <div class="text-xs uppercase font-semibold opacity-60">Bears of a fever</div>
-                        </div>
-                        <p class="list-col-wrap text-xs">
-                        "Bears of a Fever" captivated audiences with its intense energy and mysterious lyrics. Its popularity skyrocketed after fans shared it widely online, earning Ellie critical acclaim.
-                        </p>
-                        <button class="btn btn-square btn-ghost">
-                            <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
-                        </button>
-                        <button class="btn btn-square btn-ghost">
-                            <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                        </button>
-                </li>
-                
-                <li class="list-row">
-                    <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/3@94.webp"/></div>
-                        <div>
-                            <div>Sabrino Gardener</div>
-                            <div class="text-xs uppercase font-semibold opacity-60">Cappuccino</div>
-                        </div>
-                        <p class="list-col-wrap text-xs">
-                        "Cappuccino" quickly gained attention for its smooth melody and relatable themes. The song’s success propelled Sabrino into the spotlight, solidifying their status as a rising star.
-                        </p>
-                        <button class="btn btn-square btn-ghost">
-                            <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
-                        </button>
-                        <button class="btn btn-square btn-ghost">
-                            <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                        </button>
-                </li>
+                </details>
+            </li>
             </ul>
         </div>
-    </div>
+
+        <div class="place-items-center">
+            <div>
+                <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+                    <table class="table">
+                        <!-- head -->
+                        <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <!-- row 1 -->
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        <!-- row 2 -->
+                        <tr>
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        <!-- row 3 -->
+                        <tr>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </form>
+    
 </x-layout>
