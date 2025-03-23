a14e100c79642e2b4ed29ee324c144a15ccb1565
Chidi <chidi.dike93@gmail.com>
2025-03-23 14:15:26 +0100

    need to still see the messages if the input is not correct and the submit button needs to redirect to home

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
     <div>
+
         <div>
             <p class="text-center font-bold text-2xl mb-5">Login</p>
         </div>
@@ -10,16 +11,25 @@
                 
                 <label class="fieldset-label">Username</label>
                 <input type="text" class="input" placeholder="z.B.: Lewis" />
+                @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
                 
                 <label class="fieldset-label">Password</label>
-                <input type="password" class="input" placeholder="Password" />
+                <input type="password" class="input" placeholder="Password" name="password" />
+                @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
 
                 <label class="fieldset-label">Confirm Password</label>
-                <input type="password" class="input" placeholder="Password" />
+                <input type="password" class="input" placeholder="Password" name="password_confirmation" />
+                @error('password_confirmation')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
                 
                 <button class="btn hover:bg-sky-400 hover:text-white hover:border-2 hover:border-blue-800 mt-4">Login</button>
             </fieldset>
         </div>
-        
+
     </div>
 </x-layout>
