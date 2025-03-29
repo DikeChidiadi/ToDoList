202122996a233e84f0bad66fa2a31253213189b0
Chidi <chidi.dike93@gmail.com>
2025-03-29 19:50:51 +0100

    struggling right now doing the add button IT DOESNT WANT TO POSITION ITSELF

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <form action="home" methode="post" class="flex flex-col space-y-12 justify-between">
         @csrf
-        <div class="absolute top-0 right-0 p-2 mr-8">
+        <div class="absolute border-2 border-rose-800 top-2 right-0 p-2 mr-8">
             <ul class="menu menu-horizontal px-1">
             <li>
                 <details>
@@ -15,14 +15,12 @@
             </li>
             </ul>
         </div>
-
-        <div class="place-items-center flex flex-col border-2 border-gray-300">
-            <div class="flex flex-col border-2 border-gray-300 rounded-full p-6 bg-white shadow-md mt-8 mb-8 w-96">
-                <div class="flex flex-col rounded-full p-6 bg-blue-500 w-10 m-auto" src="{{ asset('img/plus.webp') }}" alt="plus">
-                    
+    
+        <div class="place-items-center flex flex-col md:flex-1 border-2 border-gray-300 p-8">
+                <input type="text" name="input" id="input" placeholder="Enter your task" class="border-2 border-gray-300 rounded-full p-2 w-80" />
+                <div>
+                    <button class="flex flex-col right-177 top-38 rounded-xl p-6 bg-blue-500 w-20 h-10 float-right hover:border-4 border-rose-800" src="{{ asset('img/plus.webp') }}" alt="plus" style="position: absolute;">Add</button>
                 </div>
-            </div>
         </div>
     </form>
-    
 </x-layout>
