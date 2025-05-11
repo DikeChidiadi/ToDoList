c3b3063f3a55a832b57a4bafebc3d87960947312
Chidi <chidi.dike93@gmail.com>
2025-05-11 18:20:13 +0200

    removed some, added some

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\LoginRegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('loginregister');
-});
+Route::get('/', [LoginRegisterController::class, 'index']);
+
 
 Route::get('/home', function () {
     return view('home');
@@ -13,4 +12,7 @@ Route::get('/home', function () {
 
 Route::view('/loginregister', 'loginregister');
 
-Route::post('/home', [LoginRegisterController::class, 'home']);
+Route::get('/home', [LoginRegisterController::class, 'home'])->name('home');
+
+Route::post('/home', [LoginRegisterController::class, 'login'])->name('login');
+Route::post('/home', [LoginRegisterController::class, 'register'])->name('register');
