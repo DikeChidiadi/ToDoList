bab4c48bb07931acd963ce04d4485210134faf3f
Chidi <chidi.dike93@gmail.com>
2025-05-17 20:53:38 +0200

    changed something so it will redirect to the home page

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
 
 Route::get('/',  function () {
     return view('loginregister');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/home', [LoginController::class, 'home'])->name('home');
+Route::post('/login', [LoginController::class, 'login']);
 
+
+
+Route::match(['get', 'post'], '/home', [RegisterController::class, 'home']);
