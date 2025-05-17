17e0833ac840f17e0e63288b8f9b20c71bb537ea
Chidi <chidi.dike93@gmail.com>
2025-05-17 11:44:05 +0200

    removed the password confirmation

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,8 +16,7 @@ return new class extends Migration
             $table->string('firstname');
             $table->string('lastname');
             $table->string('username')->unique(); //unique makes sure that the username is unique in the database and cannot be duplicated
-            $table->string('password');
-            $table->string('password_confirmation'); //This field is used to confirm the password during registration            
+            $table->string('password');            
             $table->rememberToken();
             $table->timestamps();
         });
