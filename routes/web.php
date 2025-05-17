8eef8c0745341da8832a80b1dfb1977f9d307fb0
Chidi <chidi.dike93@gmail.com>
2025-05-17 11:24:53 +0200

    Removed some of the routings

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
-use App\Http\Controllers\LoginRegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [LoginRegisterController::class, 'index']);
-
+Route::get('/',  function () {
+    return view('loginregister');
+});
 
 Route::get('/home', function () {
     return view('home');
 });
 
-Route::view('/loginregister', 'loginregister');
-
-Route::get('/home', [LoginRegisterController::class, 'home'])->name('home');
-
-Route::post('/home', [LoginRegisterController::class, 'login'])->name('login');
-Route::post('/home', [LoginRegisterController::class, 'register'])->name('register');
