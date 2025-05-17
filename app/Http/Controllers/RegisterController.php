dc6e2153d3216a5947fa6e397003e2908e9aacec
Chidi <chidi.dike93@gmail.com>
2025-05-17 20:52:22 +0200

    Made the Register Controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,4 +9,30 @@ class RegisterController extends Controller
     public function index() {
         return view('home');
     }
+
+    function register(Request $request) {
+        $request->validate([
+            'firstname' => 'required|min:3|max:20',
+            'lastname' => 'required|min:3|max:20',
+            'password' => 'required|min:6',
+        ]);
+
+        session()->put('inputData', [
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'password' => $request->password,
+        ]);
+        return redirect('/home')->with('success', 'You have been registered');
+    }
+
+    function home() {
+        $inputData = session()->get('inputData');
+        
+        return view('home', ['inputData' => $inputData]);
+    }
+
+    function logout(Request $request) {
+        $request->session()->flush();
+        return redirect('/')->with('success', 'You have been logged out');
+    }
 }
