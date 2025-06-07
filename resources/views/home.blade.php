8214fd017c3d129f3fb617dfe2f059f6aedffbe8
Chidi <chidi.dike93@gmail.com>
2025-06-07 16:21:06 +0200

    remade the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,35 @@
 <x-layout>
-    <form action="home" methode="post" class="flex flex-col space-y-12 justify-between">
+    <form action="{{ route('home') }}" method="POST" class="flex flex-col space-y-12 justify-between">
         @csrf
-        <div class="absolute border-2 border-rose-800 top-2 right-0 p-2 mr-8">
-            <ul class="menu menu-horizontal px-1">
-            <li>
-                <details>
-                @auth
-                <summary>Hi, {{ Auth::user()->name }}</summary>
-                @endauth
-                <ul class="bg-base-100 rounded-t-none p-2">
-                    <li><a>Logout</a></li>
-                </ul>
-                </details>
-            </li>
-            </ul>
-        </div>
-    
-        <div class="place-items-center flex flex-col md:flex-1 border-2 border-gray-300 p-8">
-                <input type="text" name="input" id="input" placeholder="Enter your task" class="border-2 border-gray-300 rounded-full p-2 w-80" />
-                <div>
-                    <button class="flex flex-col right-177 top-38 rounded-xl p-6 bg-blue-500 w-20 h-10 float-right hover:border-4 border-rose-800" src="{{ asset('img/plus.webp') }}" alt="plus" style="position: absolute;">Add</button>
+        <div class="place-items-center">
+            <fieldset class="fieldset bg-base-200 border p-4 border-base-300 rounded-box w-xs">
+                <div class="mb-4 flex items-end space-x-2">
+                    <div class="flex-1">
+                        <label class="block mb-1 font-semibold" for="todo">To-Do</label>
+                        <input type="text" name="todo" id="todo" class="input" placeholder="What do you need to do?" required />
+                    </div>
+                    <button type="submit" class="btn btn-primary h-10">Add</button>
                 </div>
+            </fieldset>
         </div>
     </form>
+
+    @if(!empty($todos))
+        <div class="mt-8 w-1/2 mx-auto">
+            <ul class="space-y-4">
+                @foreach($todos as $id => $item)
+                    <li class="flex justify-between items-center bg-white p-4 rounded shadow">
+                        <span>{{ $item['todo'] }}</span>
+                        <div class="space-x-2">
+                            <button class="btn btn-sm btn-secondary" disabled>Edit</button>
+                            <form action="{{ route('todo.delete', $id) }}" method="POST" class="inline">
+                                @csrf
+                                <button type="submit" class="btn btn-sm btn-error">Delete</button>
+                            </form>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </x-layout>
