09f0009d782fca61dc0a7fdbacf27050c11e69fe
Chidi <chidi.dike93@gmail.com>
2025-06-08 19:35:03 +0200

    added the route to the form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,16 +1,17 @@
 <x-layout>
-    <form action="register" method="post">
+    <form action="{{ route('register') }}" method="post">
         @csrf
+        
         <div class="flex place-content-center mt-15">
 
-            <div class="place-items-center mr-15 p-8 rounded-xl bg-sky-100">
+            <div class="place-items-center mr-15 p-8 rounded-xl shadow-xl bg-sky-100">
 
                 <div>
-                    <p class="text-center font-bold text-2xl mb-5">Register</p>
+                    <p class="text-center font-bold text-2xl">Register</p>
                 </div>
                 
                 <div class="place-items-center">
-                    <fieldset class="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+                    <fieldset class="fieldset w-xs bg-sky-100 p-4 rounded-box">
                         <legend class="fieldset-legend"></legend>
                         
                         <label class="fieldset-label">Username</label>
@@ -25,7 +26,7 @@
                             <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                         @enderror
                         
-                        <button class="btn hover:bg-blue-400 hover:text-white hover:border-2 hover:border-blue-900 mt-4">Register</button>
+                        <button class="btn hover:bg-blue-400 hover:text-white hover:border-2 hover:border-blue-900 shadow-lg hover:shadow-blue-900 mt-4">Register</button>
                     </fieldset>
                 </div>
             </div>
