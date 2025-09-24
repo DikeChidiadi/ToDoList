b961d6bc0e1dbe2b70412de920b37d6d28896411
Chidi <chidi.dike93@gmail.com>
2025-09-24 17:59:59 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\TodosController;
+use App\Http\Controllers\LoginController;
 
 
-Route::get('/',  function () {
-    return view('register');
-});
-
-Route::post('/register', [RegisterController::class, 'register'])->name('register');
-Route::get('/home', [TodosController::class, 'index'])->name('home');
+Route::get('/', [RegisterController::class, 'create'])->name('register.form');
+Route::post('/register', [RegisterController::class, 'store'])->name('register');
+Route::get('/home', [TodosController::class, 'index'])->name('home')->middleware('auth');
 Route::post('/todos', [TodosController::class, 'store'])->name('todos.store');
-Route::post('/delete/{id}', [TodosController::class, 'delete'])->name('todo.delete');
-Route::post('/todos/{id}/edit', [TodosController::class, 'edit'])->name('todos.edit');
-Route::post('/todos/{id}/update', [TodosController::class, 'update'])->name('todos.update');
-Route::post('/todos/{id}/toggle', [TodosController::class, 'toggle'])->name('todos.toggle');
+Route::post('/delete/{todo}', [TodosController::class, 'delete'])->name('todo.delete');
+Route::post('/todos/{todo}/edit', [TodosController::class, 'edit'])->name('todos.edit');
+Route::post('/todos/{todo}/update', [TodosController::class, 'update'])->name('todos.update');
+Route::post('/todos/{todo}/toggle', [TodosController::class, 'toggle'])->name('todos.toggle');
+
+// Login Routes
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+// Logout Route
+Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
